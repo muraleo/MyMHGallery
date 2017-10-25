4cdcf2596e8422956b579b567793b5f40f1c12bc
muraleo <muraleo80923@gmail.com>
2017-10-24 22:10:07 -0400

    finish add new methos

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+var mhshots = [
+	{name: "New Weapon", img:"https://i.redd.it/edn8hvecwx4y.png"},
+	{name: "Final Boss", img: "https://i.ytimg.com/vi/u7tuDneIGwo/maxresdefault.jpg"},
+	{name: "MHXX", img:"http://images.nintendolife.com/news/2016/12/gallery_feast_your_eyes_on_a_whole_lot_of_monster_hunter_xx_double_cross_screens/attachment/0/original.jpg"}
+]
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
-	res.send("lalal");
+	res.render("landing");
+})
+
+app.get("/mhshots", function(req, res){
+	res.render("mhshots", {mhshots: mhshots});
+})
+
+app.post("/mhshots", function(req, res){
+	mhshots.push({name: req.body.name, img: req.body.image});
+	res.redirect("/mhshots");
+})
+
+app.get("/mhshots/new", function(req, res){
+	res.render("new");
 })
 
 app.listen(3000, function(){
-	console.log("MyYelpCamp has connected!");
+	console.log("MyMHgallery has connected!");
 })
